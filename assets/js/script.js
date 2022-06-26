28a41bce8f1348a8c9c269e8d0f19305926717eb
loudagnese <louis.dagnese@gmail.com>
2022-06-26 15:14:25 -0700

    starting API calls, finish css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 // variable declarations
 
-// api call handler
+// api call handlers
+function getCityLatLong(city, state) {
+    var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},usa&limit=1&appid=${config.myKey}`;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (result) {
+                    console.log(result);
+                })
+            } else {
+                console.log("Response was NOT ok");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
 
 // element creation handler for api data
     // needs error handling
 
 // handler for localStorage of past searches
 
-// load localStorage searches 
+// load localStorage searches
     // after everysearch and on load
 
-// load localstorage    
+// load localstorage
 // event listeners
